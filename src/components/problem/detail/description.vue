ae588af9ebc4e0f358d02ee16529ef0dbced0656
LaPluses <xipersup@gmail.com>
2018-11-01 03:07:59 +0800

    Swap position of problem note and problem constraints

--- src/components/problem/detail/description.vue
+++ src/components/problem/detail/description.vue
@@ -28,14 +28,14 @@
 			</tr>
 		</table>
 		<h2
+			v-if="problem.constraints"
+			class="headline mb-3">Constraints</h2>
+		<h2
 			v-if = "problem.note"
 			class = "headline mb-3">Note</h2>
 		<div
 			v-mixrend = "problem.note"
 			class="mb-3" />
-		<h2
-			v-if="problem.constraints"
-			class="headline mb-3">Constraints</h2>
 		<div
 			v-mixrend = "problem.constraints"
 			class="mb-3" />
