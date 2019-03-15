49592b7c8887f44ed86c9dd27df3e2913702b6b7
LaPluses <xipersup@gmail.com>
2019-03-15 23:21:19 +0800

    [Contest]: Update contest submission.

--- src/components/contest/submission/detail.vue
+++ src/components/contest/submission/detail.vue
@@ -6,7 +6,36 @@
 
 
 <script>
+
+import { establishWebSocketConnection, closeWebSocketConnection } from '@/components/status/detail/connection';
+
+
 export default {
 
+	data() {
+		return {
+			isLoading: false,
+			isError: false,
+			ws: null,
+		};
+	},
+
+
+	methods: {
+		start(pk) {
+			if (pk === null || pk === undefined || !pk) {
+				return;
+			}
+			let pro = Promise.resolve();
+			this.isLoading = true;
+			this.isError = false;
+			if (!this.ws) {
+				pro = closeWebSocketConnection(this.ws);
+			}
+			pro.then(() => {
+				this.ws = establishWebSocketConnection(pk);
+			});
+		},
+	},
 };
 </script>
