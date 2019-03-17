b09ca5a04bacee4a6bc27846584670cb3453f608
LaPluses <xipersup@gmail.com>
2019-03-17 19:17:10 +0800

    [Contest]: Add problem routing.

--- src/components/contest/detail/submission.vue
+++ src/components/contest/detail/submission.vue
@@ -188,8 +188,11 @@ export default {
 			maxPage: 0,
 			titleToIdx: new Map(),
 			problemList: [],
+<<<<<<< HEAD
 			dialog: false,
 			queryPk: null,
+=======
+>>>>>>> [Contest]: Add problem routing.
 		};
 	},
 
